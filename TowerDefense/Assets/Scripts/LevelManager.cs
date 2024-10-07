53f1f6e95a6fdc0ae4d36188dc88d9162bb9e459
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-07 14:49:08 -0300

    criação do level base

--- TowerDefense/Assets/Scripts/LevelManager.cs
+++ TowerDefense/Assets/Scripts/LevelManager.cs
@@ -2,21 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:TowerDefense/Assets/Scripts/LevelManager.cs
-public class LevelManager : MonoBehaviour
-========
-public class level : MonoBehaviour
->>>>>>>> Inimigos:TowerDefense/Assets/Scripts/level.cs
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+public class LevelManager : MonoBehaviour
+{ 
+    public static LevelManager main;
+    public Transform[] caminhos; //onde o inimigo vai passar
+    public Transform começo;// onde aparece o inimigo
+    private void Awake()
     {
-        
+        main = this;
     }
 }
