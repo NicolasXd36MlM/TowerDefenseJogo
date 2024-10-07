b946e1963786614cd860ddd311791432dee7a6e6
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-07 14:13:33 -0300

    criar o fundo e o caminho

--- TowerDefense/Assets/Scripts/Torres/TorreAgua.cs
+++ TowerDefense/Assets/Scripts/Torres/TorreAgua.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TorreAgua : MonoBehaviour
+public class TorreAgua : TorresPai
 {
     // Start is called before the first frame update
     void Start()
---
+++ TowerDefense/Assets/Scripts/Torres/TorresPai.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class TorresPai : MonoBehaviour, ITorres
+{
+    public int vidaBase;
+
+    public int velocidadeBase;
+
+    public bool atirrarBase;
+
+    public void atirrar()
+    {
+    
+    }
+
+    public void velocidade()
+    {
+       
+    }
+
+    public void vida()
+    {
+       
+    }
+}
