651e88d5cfed00c1529014bb1c4b4891fcd05408
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-07 14:55:05 -0300

    adição das variaveis e declarando o objetivo

--- TowerDefense/Assets/Scripts/Inimigos/InimigoPai.cs
+++ TowerDefense/Assets/Scripts/Inimigos/InimigoPai.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class InimigoPai : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D corpo;
+    [SerializeField] private float velocidade = 1f;
+
+    private Transform objetivo; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+          objetivo = LevelManager.main.Caminhos
     }
 
     // Update is called once per frame
