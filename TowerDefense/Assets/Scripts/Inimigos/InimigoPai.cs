ecefd36649bcf00d1383bad756e0c8adb104bccf
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-07 14:58:15 -0300

    definir objetivo e começo do If

--- TowerDefense/Assets/Scripts/Inimigos/InimigoPai.cs
+++ TowerDefense/Assets/Scripts/Inimigos/InimigoPai.cs
@@ -7,17 +7,17 @@ public class InimigoPai : MonoBehaviour
     [SerializeField] private Rigidbody2D corpo;
     [SerializeField] private float velocidade = 1f;
 
-    private Transform objetivo; 
-    
+    private Transform objetivo;
+    private int caminhoIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-          objetivo = LevelManager.main.Caminhos
+        objetivo = LevelManager.main.caminhos[caminhoIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(objetivo.position, transform.position) < velocidade) { }
     }
 }
