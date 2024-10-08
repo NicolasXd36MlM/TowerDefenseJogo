7fb226f65629e3ebfb53c72b53290afb8f1f3dd0
NicolasXd36MlM <yukiminko519@gmail.com>
2024-10-08 14:12:35 -0300

    spawnar Inimigos

--- TowerDefense/Assets/Scripts/SpawnDeInimigo.cs
+++ TowerDefense/Assets/Scripts/SpawnDeInimigo.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class SpawnDeInimigo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] 
+    private GameObject[] prefabInimigo;
+
+    [SerializeField]
+    private int BaseDoInimigo;
+    [SerializeField]
+    private float quantidadeDeInimgoPerSec = 0.5f;
+    [SerializeField]
+    private float intervaloDeTempo = 3f;
+    [SerializeField]
+    private float dificuldade = 0.60f;
+
+    private int ondaAtual = 1;
+    private float tempoDaUltimaOnda;
+    private int inimigosVivos;
+    private int inimigosEsperando;
+    private bool nascendoIni = false;
+
+    private void Update()
     {
+        if (!nascendoIni) return; 
+        tempoDaUltimaOnda += Time.deltaTime;
+
+        if (tempoDaUltimaOnda >= (1f / quantidadeDeInimgoPerSec))
+            Debug.Log("Nasceu!");
+        {
         
+        
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OndaInicial()
     {
-        
+        inimigosEsperando = EniPorOnda();
+    }
+
+    private int EniPorOnda() 
+    {
+        return Mathf.RoundToInt(BaseDoInimigo * Mathf.Pow(ondaAtual, dificuldade));
+    
     }
 }
